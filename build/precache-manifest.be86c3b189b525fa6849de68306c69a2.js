ec286784fff3c480d9fb70fa832d7ee24ef503b8
Nushaine <54187581+Nushaine@users.noreply.github.com>
2020-09-20 00:57:05 -0400

    final changes to personal manual

---
+++ build/precache-manifest.be86c3b189b525fa6849de68306c69a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a265c9d9d86c39eed7d33fa69c5ac14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f339f4f9bcc8eeda0ca",
+    "url": "/static/css/main.be6b3c3c.chunk.css"
+  },
+  {
+    "revision": "3c895cad706bb71e7b7e",
+    "url": "/static/js/2.16e7a2b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.16e7a2b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f339f4f9bcc8eeda0ca",
+    "url": "/static/js/main.f9a3e68d.chunk.js"
+  },
+  {
+    "revision": "0eedfca001aee7c0a48a",
+    "url": "/static/js/runtime-main.cfa2c8c0.js"
+  }
+]);
