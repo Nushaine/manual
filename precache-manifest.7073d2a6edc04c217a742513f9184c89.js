9370ac0ab62c7939d50b146902e6382d3e8adc38
Nushaine <54187581+Nushaine@users.noreply.github.com>
2020-09-20 01:10:21 -0400

    Updates

---
+++ precache-manifest.7073d2a6edc04c217a742513f9184c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d155f319d23afa62af7b1e456c78eb4",
+    "url": "/manual/index.html"
+  },
+  {
+    "revision": "7deef91f9c50fbccf951",
+    "url": "/manual/static/css/main.be6b3c3c.chunk.css"
+  },
+  {
+    "revision": "22af04ad29b5c4bd2347",
+    "url": "/manual/static/js/2.8a9f0242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manual/static/js/2.8a9f0242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7deef91f9c50fbccf951",
+    "url": "/manual/static/js/main.b454e4e2.chunk.js"
+  },
+  {
+    "revision": "3fc1c97b8345c051634c",
+    "url": "/manual/static/js/runtime-main.6af23faa.js"
+  }
+]);
