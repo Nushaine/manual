5d67b1409ef2a663006f99b5df4b94e2e6333ca0
Nushaine <54187581+Nushaine@users.noreply.github.com>
2020-09-20 00:48:42 -0400

    final changes to personal user manual

--- src/App.js
+++ src/App.js
@@ -1,26 +1,176 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "../src/App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Manual extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <div className="main-contents">
+          <h1 style={{fontSize: '50px'}}>
+            My Personal User Manual
+          </h1>
+          <p>Welcome to my Personal User Manual. Here, I'll cover all aspects of myself, from my core values to my work habits. I hope you'll get some value from this, and maybe laugh at a couple of my sarcastic jokes (if you can find them). Okay, let's get started: </p>
+          <h1>Here, I'll cover: </h1>
+          <p style={{fontSize: '22px'}}>- A Rundown of My Personality</p>
+          <p style={{fontSize: '22px'}}>- Strengths/Weaknesses</p>
+          <p style={{fontSize: '22px'}}>- My Personal Values</p>
+          <p style={{fontSize: '22px'}}>- What I Value in Other People</p>
+          <p style={{fontSize: '22px'}}>- Technical + Non-Technical Skills</p>
+          <p style={{fontSize: '22px'}}>- Overview of my Schedule</p>
+
+
+          <h1>A Rundown of My Personality</h1>
+          <p style={{fontSize: '22px'}}>So I believe to get the best overlook of our own personality, we need to consider both our own opinion and an unbiased 3rd party take. In my case, I'm going to use personality assesments, like the famously used Myers-Briggs, to get that 3rd party perspective. </p>
+          <p style={{fontSize: '22px'}}>I'll start off with my <strong style={{fontStyle: 'italic'}}>personal opinion</strong>. First of all, what is personality:</p>
+          <p style={{fontSize: '22px', fontStyle: 'italic',  marginLeft: '20px', marginRight: '20px'}}> Personality refers to individual differences in characteristic patterns of thinking, feeling and behaving.</p>
+          <p style={{fontSize: '22px'}}>Okay, so considering this definition (literally the first one from Google), I can split my assessment into 3 parts:</p>
+          <p style={{fontSize: '22px'}}>- <strong>How I think: </strong>So, I'd say I'm more of a creative and abstract thinker with a touch of an analytical side. As much as I really like math, I was always more naturally talented in designing and ideating than logical analysis. I know, not your average brown guy, eh? Also, I tend to think much more clearly when I work alone than when with a group, however I still enjoy having discussions and brainstorming sessions with others.</p>
+          <p style={{fontSize: '22px'}}>- <strong>How I behave: </strong>Towards others, I tend to be pretty empathetic and forgiving. I really take the <span style={{fontStyle: 'italic'}}>golden rule</span> to heart, since I believe in helping people rather than shitting on them. Also, I should mention I'm pretty quiet when it comes to meetings and discusions with people I'm not close to. </p>
+          <p style={{fontSize: '22px'}}>- <strong>How I feel </strong>: I really liked the format Ray Dalio uses to dsecribe how people are wired, so I'll use that instead of me just blabbering on for a whole paragraph. </p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Introverted vs Extroverted</strong>: I can be both, more on the introverted side though.</p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Thinking vs Feeling</strong>: More on the <span>feeling</span> and <span>emotional</span> side. When I say "feeling", I mean focusing on the experience/quality of something vs reasoning completely off stats and data.</p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Intuiting vs Sensing</strong>: I'm more of an intuitive (big-picture) thinker than someone who is more detail-oriented. </p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Planning vs Percieving</strong>: More of a perciever than a planner (more spontaneous than structured), however I do like having some structure, otherwise I tend to focus on the wrong things.</p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Creator vs Advancer vs Refiner vs Executor</strong>: I'm kind of a combination of the creator and the refiner. In other words, I'm good at generating creative ideas and good at challenging current ideas. However, I'm not the greatest at execution and communicating ideas (advancer). </p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Creator vs Advancer vs Refiner vs Executor</strong>: I'm kind of a combination of the creator and the refiner. In other words, I'm good at generating creative ideas and good at challenging current ideas. However, I'm not the greatest at execution and communicating ideas (advancer). </p>
+          <p style={{fontSize: '16px', marginBottom: '5px'}}>- <strong>Focus on Tasks vs Goals</strong>: I focus much more on goals than individual tasks. </p>
+          <br></br>
+          <p style={{fontSize: '22px'}}>- My <strong style={{fontStyle: 'italic'}}>Myers-Briggs</strong> personality type: (*insert here*).</p>
+
+
+
+
+
+          <h1>Strengths/Weaknesses</h1>
+          <p style={{fontSize: '22px'}}>Starting off with my <strong style={{fontStyle: 'italic'}}>strengths</strong>:</p>
+          <p style={{fontSize: '22px'}}>- <strong>Dedication:</strong> Whenever I start something, especially a group project, I have this relentless desire to push through to the end at all costs. Its kind of like a go all in or go home mentality.</p>
+          <p style={{fontSize: '22px'}}>- <strong>Creativity:</strong> I have a natural tendency to see the big picture, come up with cool ideas and create product ideas/user journeys, whether that be for aomething like a mobile app all the way up to a moonshot project.</p>
+          <p style={{fontSize: '22px'}}>- <strong>Teamwork:</strong> Whenever lead teams, I create a positive and friendly dynamic where evreyone is treated equitably and held accountable for. I'm pretty good getting the best out of people and motivating the whole team while keeping an upbeat atmosphere.</p>
+          <p style={{fontSize: '22px'}}>- <strong>Positivity:</strong> I've gotten mad only a couple times in my life, and that was when I forgot to save my code (kind of a bruh moment). I'm pretty laid back, happy and chill most of the time. Honestly, I'm not too sure why. I guess its my nature?</p>
+          <p style={{fontSize: '22px'}}>I'd say those are my main strengths. I'll keep adding to this list as I discover more about myself and talk to more people. Now, on to my <strong style={{fontStyle: 'italic'}}>weaknesss</strong>:</p>
+          <br></br>
+          <p style={{fontSize: '22px'}}>- <strong>Over-Optimism:</strong> I often inflate what I think others are really capable of vs what they're actually capable of and overestimate time it takes to accomplish a certain task. </p>
+          <p style={{fontSize: '22px'}}>- <strong>Triviality:</strong> I do this so much its actually funny. I ALWAYS get caught up in the small, sometimes irrelevant details without focusing on what's important. Sometimes its a good thing, but normally it causes me to fall behind. </p>
+          <p style={{fontSize: '22px'}}>- <strong>Self Confidence:</strong> Something I really need to work on, but I often put myself down or see myself as 'worse' or less-competent than what I actually am. It never really affects my productivity, but it does <span style={{fontStyle: 'italic'}}>slightly</span> affect my personal morale. </p>
+          <p style={{fontSize: '22px'}}>- <strong>Overthinking:</strong> Sometimes, I tend to overthink decisions rather than be practical and simple. Its something I'm really trying to work on, and definetly call me out if you sense that I'm doing this. </p>
+
+
+
+          <h1>My Personal Values</h1>
+          <p style={{fontSize: '22px'}}>- <strong>Honesty:</strong> I believe honesty is something that every person should have, and I always make sure I tell the truth even if I'm really tempted to lie. Simply put, no ones likes dishonest people, and there's nothing good from boing dishonest, so why lie?</p>
+          <p style={{fontSize: '22px'}}>- <strong>Humility:</strong> I find by being humble, you don't only keep your ego from overexpanding, people simply appreciate you so much more than if you shove all your achievements in their face. I can't say  I'm humble 100% of the time, but overall I do my best to override pride and ego with humility.</p>
+          <p style={{fontSize: '22px'}}>- <strong>Chill:</strong> I can be intense sometimes, but I find staying calm and relaxed helps me reason out more logically than emotionally, and also it minimizes overreactings to seemingly irrelevant things. I'm not sure if its exactly a <span style={{fontStyle: 'italic'}}>value</span>, but its something I try to be intentional about on a regular basis.</p>
+          <p style={{fontSize: '22px'}}>- <strong>Authenticity:</strong> Its pretty underrated in the real world, but I constantly make sure I'm doing things because I actually want to, and not to impress someone else.  </p>
+          <p style={{fontSize: '22px'}}>- <strong>Passion:</strong> Personally, I find it important to enjoy the process just as much as the end result. Its something I really value, from when I build relationships to choosing projects to work on.</p>
+
+
+
+          <h1>What I Value in Others</h1>
+          <p style={{fontSize: '22px'}}>- <strong>Honesty:</strong> Like I mentioned before, honesty is KEY when working in teams, or  in any relationship for that matter. When working in teams, I need to trust that you'll be honest about what you've done, and what you think you can handle.</p>
+          <p style={{fontSize: '22px'}}>- <strong>Humility:</strong> I really value people who accomplish a lot and don't shove it in everyone else's face. Being humble keeps a good team dynamic and gives you the respect and trust of everyone else on your team.  </p>
+          <p style={{fontSize: '22px'}}>- <strong>Creativity:</strong> I appreciate people who can think outside of the box and who can challenge our current process/solution. </p>
+          <p style={{fontSize: '22px'}}>- <strong>Friendly:</strong> As much as I care about the end goal, I also want to enjoy the process. I want to see you more like a <span style={{fontStyle: 'italic'}}>friend</span>, and less like a <span style={{fontStyle: 'italic'}}>colleage</span> or <span style={{fontStyle: 'italic'}}> that guy I work with who I barely know anything about</span>.</p>
+
+
+
+
+          <h1>Technical + Non-Technical Skills</h1>
+          <p style={{fontSize: '22px'}}>Let's start with my <strong style={{fontStyle: 'bold'}}>technical</strong> skills: </p>
+          <p style={{fontSize: '15px', fontStyle: 'italic'}}>p.s. As soon as I get my website up I'll link my work according to each technical skill</p>
+          <br></br>
+          <p style={{marginBottom: '5px'}}>AI/Deep Learning</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Build Deep Learning Models in Tensorflow/Keras/Pytorch</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Implement Deep Learning Research Papers</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Build Practical Web/Mobile Apps using Deep Learning Models</p>
+
+          <br></br>
+
+          <p style={{marginBottom: '5px'}}>Web Development</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Build Websites/Web Apps in React.js, Next.js or Gatsby.js</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Build API's using Python's Flask framework</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Design + Build databases in MySQL <span style={{fontStyle: 'italic', fontSize: '18px'}}>(honestly, not super experienced with this)</span></p>
+
+          <br></br>
+
+          <p style={{marginBottom: '5px'}}>Mobile App Development</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Build mobile apps in React Native</p>
+          <br></br>
+
+          <p style={{marginBottom: '5px'}}>UI/UX Design</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Build user journeys + web/mobile mockups in Figma</p>
+          <br></br>
+
+          <p style={{marginBottom: '5px'}}>Other</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Storyboard + Design slide decks that are actually good</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Edit Videos in Adobe Premiere Pro and Filmora</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Write boring articles on medium.com</p>
+
+          <br></br>
+          <br></br>
+
+          <p style={{fontSize: '22px'}}>Now continuing to <strong style={{fontStyle: 'bold'}}>non-technical</strong> skills: </p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Learn + Adapt at a Fast Pace</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Creative problem-solving</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Extremely Flexible</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Dependable + Open-Minded</p>
+          
+          <br></br>
+          <p style={{fontSize: '22px'}}>Now, to get an even better idea of who I am, here are some soft-skills which I am <strong>not super great at: </strong></p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Communicating Ideas Clearly</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Staying Organized + prioritizing</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Public Speaking, <span style={{fontSize: '15px', fontStyle: 'italic'}}>although I can argue that if I'm well prepared with a good deck I will present well. I'm just bad at improv :(</span></p>
+          <p style={{fontSize: '22px'}}>To improve my organizational skills, I'm working on creating a better schedule for myself. And for communication and public speaking (improv), I guess that just comes with more practice. </p>
+
+
+
+
+
+
+          <h1>Overview of my schedule</h1>
+          <p style={{fontSize: '22px'}}>Honestly, my schedule keeps changing so I doubt that the one I'm using when I write this will be the <span style={{fontStyle: 'italic'}}>exact</span> one I'm using right now, but it will probably be something similar. </p>
+          <br></br>
+          <p style={{marginBottom: '5px'}}>Weekdays</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>7-7:30 am</strong>: Wake-up</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>7:30am- 4pm</strong>: May Respond, but not right away. (in school right now)</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>4pm - 5pm</strong>: Downtime, optimal time for meetings</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>5pm - 8pm</strong>: I'll probably respond quickly, but I'm in deep work right now.</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>8pm - 10pm</strong>: Very likely respond quickly, I'm doing light work right now</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>10pm - 12pm</strong>: May/may not respond. Finishing up work + getting ready for bed.</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>12 am</strong>: sleep</p>
+          <br></br>
+          <p style={{marginBottom: '5px'}}>Weekends</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- Honestly, I don't have a set structure for my weekends, since there's always random shit happening. Call/message me whenever, I'll probably respond within an hour or so. </p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>8-9 am</strong>: Wake-up</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>1 am</strong>: Sleep</p>
+
+          <br></br>
+          <br></br>
+          
+          <p style={{fontSize: '22px'}}>Well, that's all I got. I hope you've got a good idea of who I am now. Feel free to contact me at:</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>Insta</strong>: @nushaine</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>Email</strong>: nushainef@gmail.com</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>Discord</strong>: Nushaine #8399</p>
+          <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>Phone</strong>: I'm too broke for a phone :(</p>
+            <p style={{fontSize: '22px', marginBottom: '5px'}}>- <strong>Other</strong>: Google Hangouts <span style={{fontSize: '18px', fontStyle: 'italic'}}>(use nushaine.ferdinand@ocsbstudent.ca)</span>, Slack</p>
+
+          <br></br>
+          <br></br>
+          <br></br>
+
+
+          <p style={{marginBottom: '50px'}}>Well, thanks for reading, and have an epic day :)</p>
+
+
+
+
+
+
+
+
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Manual;
